e3a26878e3b95f3b6e270ba1f8e573f76e229084
purna-muddangula <smileyramya05@gmail.com>
2024-09-23 15:38:04 +0530

    removed unnecessary data

--- main.tf
+++ main.tf
@@ -6,46 +6,11 @@ provider "aws" {
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "buckityy"  # Replace with a unique bucket name
-  acl    = "private"  # Define the Access Control List (ACL)
-
+  
   tags = {
     Name        = "My S3 Bucket"
     Environment = "Dev"
   }
 }
 
-# Optional: Enable versioning
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.my_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
 
-# Optional: Enable server-side encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-# Optional: Add a bucket policy
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "s3:GetObject"
-        Effect    = "Allow"
-        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
-        Principal = "*"
-      }
-    ]
-  })
-}
